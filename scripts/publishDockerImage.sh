42a0e76db481a399b36e6b3a2cc624cea15c4476
Matt Smith <mattonfoot@gmail.com>
2017-11-09 11:02:12 +0000

    update docker login params

--- scripts/publishDockerImage.sh
+++ scripts/publishDockerImage.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-APPLICATION_NAME=${1}
-BUILD_VERSION=${2}
+PATH=${1}
+APPLICATION_NAME=${2}
+BUILD_VERSION=${3}
 
-CIRCLE_BUILD_NUM=${3}
-CIRCLE_BUILD_URL=${4}
-CIRCLE_SHA1=${5}
+CIRCLE_BUILD_NUM=${4}
+CIRCLE_BUILD_URL=${5}
+CIRCLE_SHA1=${6}
 
-DOCKER_EMAIL=${6}
-DOCKER_USERNAME=${7}
-DOCKER_PASSWORD=${8}
+DOCKER_EMAIL=${7}
+DOCKER_USERNAME=${8}
+DOCKER_PASSWORD=${9}
 
 cd $PWD
 
-docker build . \
-  --tag "mojdigitalstudio/$APPLICATION_NAME:$BUILD_VERSION" \
+IMAGE_TAG="mojdigitalstudio/$APPLICATION_NAME:$BUILD_VERSION"
+
+docker build $PATH \
+  --tag $IMAGE_TAG \
   --label "maintainer=noms-studio-webops@digital.justice.gov.uk" \
   --label "build.number=$CIRCLE_BUILD_NUM" \
   --label "build.url=$CIRCLE_BUILD_URL" \
   --label "build.gitref=$CIRCLE_SHA1"
-docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push mojdigitalstudio/$APPLICATION_NAME:$BUILD_VERSION
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+docker push $IMAGE_TAG
