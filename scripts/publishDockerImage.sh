41390d594404827a2ddeea1e648f944394dcbfa1
Matt Smith <mattonfoot@gmail.com>
2017-11-09 11:17:34 +0000

    remove path param from publish

--- scripts/publishDockerImage.sh
+++ scripts/publishDockerImage.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-PATH=${1}
-APPLICATION_NAME=${2}
-BUILD_VERSION=${3}
+APPLICATION_NAME=${1}
+BUILD_VERSION=${2}
 
-CIRCLE_BUILD_NUM=${4}
-CIRCLE_BUILD_URL=${5}
-CIRCLE_SHA1=${6}
+CIRCLE_BUILD_NUM=${3}
+CIRCLE_BUILD_URL=${4}
+CIRCLE_SHA1=${5}
 
-DOCKER_EMAIL=${7}
-DOCKER_USERNAME=${8}
-DOCKER_PASSWORD=${9}
+DOCKER_EMAIL=${6}
+DOCKER_USERNAME=${7}
+DOCKER_PASSWORD=${8}
 
 cd $PWD
 
 IMAGE_TAG="mojdigitalstudio/$APPLICATION_NAME:$BUILD_VERSION"
 
-docker build $PATH \
+docker build . \
   --tag $IMAGE_TAG \
   --label "maintainer=noms-studio-webops@digital.justice.gov.uk" \
   --label "build.number=$CIRCLE_BUILD_NUM" \
   --label "build.url=$CIRCLE_BUILD_URL" \
   --label "build.gitref=$CIRCLE_SHA1"
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 docker push $IMAGE_TAG
