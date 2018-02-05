5fa4ce9c0133adb197825a9bdb6c6ba89ea15d41
mikejackson <michael.jackson@digital.justice.gov.uk>
2018-02-05 11:06:09 +0000

    Add some logging around user lookup

--- src/main/java/uk/gov/justice/digital/delius/service/wrapper/UserRepositoryWrapper.java
+++ src/main/java/uk/gov/justice/digital/delius/service/wrapper/UserRepositoryWrapper.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.delius.service.wrapper;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.justice.digital.delius.jpa.national.entity.User;
@@ -8,8 +9,10 @@ import uk.gov.justice.digital.delius.jpa.oracle.annotations.NationalUserOverride
 import uk.gov.justice.digital.delius.service.NoSuchUserException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
+@Slf4j
 public class UserRepositoryWrapper {
 
     private final UserRepository userRepository;
@@ -21,7 +24,10 @@ public class UserRepositoryWrapper {
 
     @NationalUserOverride
     public User getUser(String userDistinguishedName) {
-        return userRepository.findByDistinguishedNameIgnoreCase(userDistinguishedName).orElseThrow(() -> new NoSuchUserException("Can't resolve user: " + userDistinguishedName));
+        log.info("Looking up user by distinguished name {}...",userDistinguishedName);
+        Optional<User> maybeUser = userRepository.findByDistinguishedNameIgnoreCase(userDistinguishedName);
+        log.info("... found {}: {}", userDistinguishedName, maybeUser.isPresent());
+        return maybeUser.orElseThrow(() -> new NoSuchUserException("Can't resolve user: " + userDistinguishedName));
     }
 
     @NationalUserOverride
