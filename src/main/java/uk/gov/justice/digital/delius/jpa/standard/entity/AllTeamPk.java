99695bdb3a599531d218f5c83d3dc4b60927dff4
mikejackson <michael.jackson@digital.justice.gov.uk>
2018-03-22 10:27:05 +0000

    Equals & Hashcode

--- src/main/java/uk/gov/justice/digital/delius/jpa/standard/entity/AllTeamPk.java
+++ src/main/java/uk/gov/justice/digital/delius/jpa/standard/entity/AllTeamPk.java
@@ -1,9 +1,12 @@
 package uk.gov.justice.digital.delius.jpa.standard.entity;
 
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Column;
 import javax.persistence.Id;
 import java.io.Serializable;
 
+@EqualsAndHashCode
 public class AllTeamPk implements Serializable {
     @Id
     @Column(name = "TRUST_PROVIDER_FLAG")
