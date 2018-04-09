27a1821e32287173399d587ff0169b5950aeb64f
mikejackson <michael.jackson@digital.justice.gov.uk>
2018-04-09 13:37:48 +0100

    Use dropwizard metrics for hikari pool

--- src/main/java/uk/gov/justice/digital/delius/DeliusOffenderAPI.java
+++ src/main/java/uk/gov/justice/digital/delius/DeliusOffenderAPI.java
@@ -1,20 +1,8 @@
 package uk.gov.justice.digital.delius;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.boot.info.BuildProperties;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @SpringBootApplication
 @Slf4j
@@ -24,30 +12,4 @@ public class DeliusOffenderAPI {
         SpringApplication.run(DeliusOffenderAPI.class, args);
     }
 
-    @Bean
-    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-        ObjectMapper objectMapper = new ObjectMapper()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-                .setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
-                .setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
-                .registerModules(new Jdk8Module(), new JavaTimeModule());
-        jsonConverter.setObjectMapper(objectMapper);
-        return jsonConverter;
-    }
-
-    @Bean
-    public ApplicationListener<ApplicationReadyEvent> buildInfoLogger() {
-        return event -> {
-            try {
-                log.info("BUILD PROPERTIES:");
-                BuildProperties buildProperties = (BuildProperties) event.getApplicationContext().getBean("buildProperties");
-                buildProperties.iterator().forEachRemaining(prop -> log.info("{} : {}", prop.getKey(), prop.getValue()));
-            } catch (NoSuchBeanDefinitionException nsbde) {
-                log.warn("No build info found! Is this a local build?");
-            }
-        };
-    }
 }
---
+++ src/main/java/uk/gov/justice/digital/delius/config/ApplicationConfig.java
@@ -0,0 +1,70 @@
+package uk.gov.justice.digital.delius.config;
+
+import com.codahale.metrics.MetricRegistry;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+@Slf4j
+public class ApplicationConfig {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
+                .setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
+                .registerModules(new Jdk8Module(), new JavaTimeModule());
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> buildInfoLogger() {
+        return event -> {
+            try {
+                log.info("BUILD PROPERTIES:");
+                BuildProperties buildProperties = (BuildProperties) event.getApplicationContext().getBean("buildProperties");
+                buildProperties.iterator().forEachRemaining(prop -> log.info("{} : {}", prop.getKey(), prop.getValue()));
+            } catch (NoSuchBeanDefinitionException nsbde) {
+                log.warn("No build info found! Is this a local build?");
+            }
+        };
+    }
+
+    @Bean
+    BeanPostProcessor hikariMetrics(MetricRegistry metricRegistry ) {
+        return new BeanPostProcessor() {
+            @Override
+            public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+                return bean;
+            }
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof HikariDataSource) {
+                    ((HikariDataSource)bean).setMetricRegistry(metricRegistry);
+                }
+                return bean;
+            }
+        };
+    }
+}
