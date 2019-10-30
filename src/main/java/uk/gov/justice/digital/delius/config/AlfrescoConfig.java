dce29d021327aee82fe1ebc219a831f66282e06c
Michael Willis <michael.willis@digital.justice.gov.uk>
2019-10-30 11:47:45 +0000

    DT-339 add in request client id custom dimensions

--- src/main/java/uk/gov/justice/digital/delius/config/AlfrescoConfig.java
+++ src/main/java/uk/gov/justice/digital/delius/config/AlfrescoConfig.java
@@ -4,12 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
-import uk.gov.justice.digital.delius.utils.W3cTracingInterceptor;
-
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 public class AlfrescoConfig {
@@ -19,11 +14,7 @@ public class AlfrescoConfig {
                                              @Value("${alfresco.baseUrl}") String baseUrl) {
         return restTemplateBuilder
                 .rootUri(baseUrl)
-                .additionalInterceptors(getRequestInterceptors())
                 .build();
     }
 
-    private List<ClientHttpRequestInterceptor> getRequestInterceptors() {
-        return Collections.singletonList(new W3cTracingInterceptor());
-    }
 }
