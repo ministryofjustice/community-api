01d3772edc68a84308a49f8164f71d9414efdbc3
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2019-11-18 13:47:31 +0000

    DT-362: added timeout to the delius rest template builder

--- src/main/java/uk/gov/justice/digital/delius/config/DeliusConfig.java
+++ src/main/java/uk/gov/justice/digital/delius/config/DeliusConfig.java
@@ -6,17 +6,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class DeliusConfig {
 
     @Bean("deliusRestTemplateWithAuth")
-    public RestTemplate alfrescoRestTemplateWithAuth(RestTemplateBuilder restTemplateBuilder,
-                                                     @Value("${delius.baseurl}") String baseUrl,
-                                                     @Value("${delius.username}") String deliusUsername,
-                                                     @Value("${delius.password}") String deliusPassword) {
+    public RestTemplate deliusRestTemplateWithAuth(final RestTemplateBuilder restTemplateBuilder,
+                                                   @Value("${delius.baseurl}") final String baseUrl,
+                                                   @Value("${delius.username}") final String deliusUsername,
+                                                   @Value("${delius.password}") final String deliusPassword,
+                                                   @Value("${delius.connectTimeout:30s}") final Duration connectTimeout,
+                                                   @Value("${delius.readTimeout:30s}") final Duration readTimeout) {
         return restTemplateBuilder
                 .rootUri(baseUrl)
                 .basicAuthentication(deliusUsername, deliusPassword)
+                .setConnectTimeout(connectTimeout)
+                .setReadTimeout(readTimeout)
                 .build();
     }
 }
