df8534b1046bb48ad6fea53ff04f33effc2266e0
Mike Halma <mike.halma@digital.justice.gov.uk>
2019-12-06 16:56:33 +0000

    DT-400 tidy up swagger doc

--- src/main/java/uk/gov/justice/digital/delius/data/api/CreatePrisonOffenderManager.java
+++ src/main/java/uk/gov/justice/digital/delius/data/api/CreatePrisonOffenderManager.java
@@ -16,11 +16,12 @@ import static java.util.Objects.isNull;
 @NoArgsConstructor
 @AllArgsConstructor
 @Slf4j
-@ApiModel(value = "Request body for assigning an offender manager to an offender. Must pass exactly one of office / officerCode (not both)")
+@ApiModel(description = "Request body for assigning an offender manager to an offender. Must pass exactly one of officer / officerCode (not both)")
 public class CreatePrisonOffenderManager {
+    // This annotation appears to be ignored by swagger docs and nobody seems to care: https://github.com/springfox/springfox/issues/2237
     @ApiModelProperty(value = "Name of offender manager. If passed then must contain both forename(s) and surname", example = "officer: {\"forenames\": \"John\", \"surname\": \"Smith\" }")
     private Human officer;
-    @ApiModelProperty(value = "Officer staff code. If not present name will be used to lookup staff member", example = "N07A001")
+    @ApiModelProperty(value = "Officer staff code. If not present officer will be used to lookup staff member", example = "N07A001")
     private String officerCode;
     @ApiModelProperty(value = "Prison institution code in NOMIS", required = true, example = "MDI")
     private String nomsPrisonInstitutionCode;
