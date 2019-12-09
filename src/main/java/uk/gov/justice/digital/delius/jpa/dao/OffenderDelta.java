eee92dc02136b2d42bc4ce38a5c2a87896eb14cb
Michael Willis <michael.willis@digital.justice.gov.uk>
2019-12-09 07:50:04 +0000

    Dt 410 Expose Delta Endpoints (#112)
    * Rename API to community API
    
    * DT-410 Expose Delta endpoints

--- src/main/java/uk/gov/justice/digital/delius/jpa/dao/OffenderDelta.java
+++ src/main/java/uk/gov/justice/digital/delius/jpa/dao/OffenderDelta.java
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.delius.jpa.dao;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,8 +13,12 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonPropertyOrder({"offenderId", "dateChanged", "action"})
 public class OffenderDelta {
+    @ApiModelProperty(value = "Offender ID", example = "232423", position = 1)
     private Long offenderId;
+    @ApiModelProperty(value = "The datetime the change occurred", example = "2019-11-27T15:12:43.000Z", position = 2)
     private LocalDateTime dateChanged;
+    @ApiModelProperty(value = "Type of delta", example = "UPSERT", allowableValues = "UPSERT,DELETE", position = 3)
     private String action;
 }
