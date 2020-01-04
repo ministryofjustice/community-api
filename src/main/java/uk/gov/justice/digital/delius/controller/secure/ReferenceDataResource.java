7b99fb14d5ccb0da4f37101072e42ef53878a51f
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2020-01-04 10:29:59 +0000

    DCS-297 Adding swagger doc to new endpoints

--- src/main/java/uk/gov/justice/digital/delius/controller/secure/ReferenceDataResource.java
+++ src/main/java/uk/gov/justice/digital/delius/controller/secure/ReferenceDataResource.java
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.delius.controller.secure;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.Authorization;
+import io.swagger.annotations.*;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import uk.gov.justice.digital.delius.controller.NotFoundException;
+import uk.gov.justice.digital.delius.controller.advice.ErrorResponse;
 import uk.gov.justice.digital.delius.data.api.KeyValue;
 import uk.gov.justice.digital.delius.data.api.ProbationArea;
+import uk.gov.justice.digital.delius.data.api.ResponsibleOfficer;
 import uk.gov.justice.digital.delius.jwt.JwtValidation;
 import uk.gov.justice.digital.delius.service.ReferenceDataService;
 
@@ -31,14 +32,43 @@ public class ReferenceDataResource {
 
     private final ReferenceDataService referenceDataService;
 
+    @ApiOperation(
+            value = "Return probation areas",
+            notes = "Accepts filtering to only return active areas")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "OK", response = KeyValue.class, responseContainer = "PAge"),
+                    @ApiResponse(code = 400, message = "Invalid request", response = ErrorResponse.class),
+                    @ApiResponse(code = 401, message = "Unauthorised", response = ErrorResponse.class),
+                    @ApiResponse(code = 403, message = "Forbidden", response = ErrorResponse.class),
+                    @ApiResponse(code = 500, message = "Unrecoverable error whilst processing request.", response = ErrorResponse.class)
+            })
     @GetMapping("/probationAreas")
-    public Page<KeyValue> getProbationAreaCodes(final @RequestParam(name = "active", required = false) boolean restrictActive) {
+    public Page<KeyValue> getProbationAreaCodes(
+            @ApiParam(name = "active", value = "Restricts to active areas only", example = "true", required = false)
+            final @RequestParam(name = "active", required = false) boolean restrictActive) {
+
         log.info("Call to getProbationAreaCodes");
         return referenceDataService.getProbationAreasCodes(restrictActive);
     }
 
+    @ApiOperation(
+            value = "Return Local delivery units for a probation area",
+            notes = "Accepts a probation area code")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "OK", response = KeyValue.class, responseContainer = "Page"),
+                    @ApiResponse(code = 400, message = "Invalid request", response = ErrorResponse.class),
+                    @ApiResponse(code = 401, message = "Unauthorised", response = ErrorResponse.class),
+                    @ApiResponse(code = 403, message = "Forbidden", response = ErrorResponse.class),
+                    @ApiResponse(code = 404, message = "Not found", response = ErrorResponse.class),
+                    @ApiResponse(code = 500, message = "Unrecoverable error whilst processing request.", response = ErrorResponse.class)
+            })
     @GetMapping(value = "/probationAreas/code/{code}/localDeliveryUnits")
-    public Page<KeyValue> getLdusForProbationCode(final @PathVariable String code) {
+    public Page<KeyValue> getLdusForProbationCode(
+            @ApiParam(name = "code", value = "Probation area code", example = "NO2", required = true)
+            final @PathVariable String code) {
+
         log.info("Call to getLdusForProbationCode");
         final var ldus = referenceDataService.getLocalDeliveryUnitsForProbationArea(code);
 
