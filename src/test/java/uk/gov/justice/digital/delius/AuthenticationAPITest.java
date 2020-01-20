ad676b1b6c0cf44d67884dd1e2576d18e0a93235
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2020-01-20 16:17:29 +0000

    DCS-309 Fixing broken test

--- src/test/java/uk/gov/justice/digital/delius/AuthenticationAPITest.java
+++ src/test/java/uk/gov/justice/digital/delius/AuthenticationAPITest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import uk.gov.justice.digital.delius.controller.advice.ErrorResponse;
@@ -222,6 +223,7 @@ public class AuthenticationAPITest {
     }
 
     @Test
+    @DirtiesContext
     public void addRole() {
 
         given()
