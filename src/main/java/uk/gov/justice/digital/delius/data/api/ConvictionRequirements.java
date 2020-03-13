9e07d29866ca8dc7b801edab3568274da7d5910b
John Evans <John.Evans@digital.homeoffice.gov.uk>
2020-03-13 15:38:57 +0000

    Pic 271 add requirements endpoint review changes (#169)
    * PIC-271: Bring Requirement lengthUnits back where available and some Swagger documentation
    
    * PIC-271: Convert test to JUnit5, exclude transient dependency to prevent warning when running test suite
    
    Co-authored-by: John Evans <john.evans@digital.justice.gov.uk>

--- src/main/java/uk/gov/justice/digital/delius/data/api/ConvictionRequirements.java
+++ src/main/java/uk/gov/justice/digital/delius/data/api/ConvictionRequirements.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.delius.data.api;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,5 +9,6 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class ConvictionRequirements {
+    @ApiModelProperty(value = "List of requirements associated with this conviction")
     private List<Requirement> requirements;
 }
--- src/main/java/uk/gov/justice/digital/delius/jpa/standard/entity/AdRequirementTypeMainCategory.java
+++ src/main/java/uk/gov/justice/digital/delius/jpa/standard/entity/AdRequirementTypeMainCategory.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -26,4 +23,8 @@ public class AdRequirementTypeMainCategory {
 
     @Column(name = "DESCRIPTION")
     private String description;
+
+    @JoinColumn(name = "UNITS_ID")
+    @OneToOne
+    private StandardReference units;
 }
