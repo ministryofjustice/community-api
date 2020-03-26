4b95a0e4dcda1a36fbb7b26fdf246d0265c59181
chris-moj <christopher.faulkner@digital.justice.gov.uk>
2020-03-26 06:09:51 +0000

    PIC-360 Ensure Junit 5 tests execute and ensure ConvictionRequirements returns empty collection properly serialised

--- src/main/java/uk/gov/justice/digital/delius/data/api/ConvictionRequirements.java
+++ src/main/java/uk/gov/justice/digital/delius/data/api/ConvictionRequirements.java
@@ -1,13 +1,19 @@
 package uk.gov.justice.digital.delius.data.api;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 
 import java.util.List;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ConvictionRequirements {
     @ApiModelProperty(value = "List of requirements associated with this conviction")
     private List<Requirement> requirements;
