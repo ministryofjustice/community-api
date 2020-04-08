7c53d6deb31a7262dc37fe667c764acb58078cb7
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-04-08 10:58:53 +0100

    DT-645 Duplicate offenders have NOMS number moved

--- src/main/java/uk/gov/justice/digital/delius/service/OffenderIdentifierService.java
+++ src/main/java/uk/gov/justice/digital/delius/service/OffenderIdentifierService.java
@@ -7,6 +7,8 @@ import org.springframework.transaction.annotation.Transactional;
 import uk.gov.justice.digital.delius.controller.NotFoundException;
 import uk.gov.justice.digital.delius.data.api.IDs;
 import uk.gov.justice.digital.delius.data.api.UpdateOffenderNomsNumber;
+import uk.gov.justice.digital.delius.jpa.standard.entity.AdditionalIdentifier;
+import uk.gov.justice.digital.delius.jpa.standard.entity.Offender;
 import uk.gov.justice.digital.delius.jpa.standard.repository.OffenderRepository;
 import uk.gov.justice.digital.delius.transformers.OffenderTransformer;
 
@@ -17,16 +19,19 @@ public class OffenderIdentifierService {
     private final OffenderTransformer offenderTransformer;
     private final OffenderRepository offenderRepository;
     private final SpgNotificationService spgNotificationService;
+    private final ReferenceDataService referenceDataService;
 
     public OffenderIdentifierService(
             @Value("${features.noms.update.noms.number}") Boolean updateNomsNumberFeatureSwitch,
             OffenderTransformer offenderTransformer,
             OffenderRepository offenderRepository,
-            SpgNotificationService spgNotificationService) {
+            SpgNotificationService spgNotificationService,
+            ReferenceDataService referenceDataService) {
         this.updateNomsNumberFeatureSwitch = updateNomsNumberFeatureSwitch;
         this.offenderTransformer = offenderTransformer;
         this.offenderRepository = offenderRepository;
         this.spgNotificationService = spgNotificationService;
+        this.referenceDataService = referenceDataService;
         log.info("NOMIS update NOMS number feature is {}", this.updateNomsNumberFeatureSwitch ? "ON" : "OFF");
     }
 
@@ -36,13 +41,30 @@ public class OffenderIdentifierService {
                 .format("Offender with crn %s not found ", crn)));
         if (updateNomsNumberFeatureSwitch) {
             if (!updateOffenderNomsNumber.getNomsNumber().equals(offender.getNomsNumber())) {
-                offender.setNomsNumber(updateOffenderNomsNumber.getNomsNumber());
-                offenderRepository.save(offender);
-                spgNotificationService.notifyUpdateOfOffender(offender);
+                doUpdateNomsNumber(updateOffenderNomsNumber.getNomsNumber(), offender);
+            } else {
+                log.info("No need to Update NOMS number since it is already set");
             }
         } else {
             log.warn("Update NOMS number will be ignored, this feature is switched off ");
         }
         return offenderTransformer.idsOf(offender);
+    }
+
+    private void doUpdateNomsNumber(String nomsNumber, Offender offender) {
+        offenderRepository.findAllByNomsNumber(nomsNumber).forEach(duplicateOffender -> {
+            duplicateOffender.setNomsNumber(null);
+            duplicateOffender.getAdditionalIdentifiers().add(AdditionalIdentifier
+                    .builder()
+                    .identifier(nomsNumber)
+                    .offender(offender)
+                    .identifierName(referenceDataService.duplicateNomsNumberAdditionalIdentifier())
+                    .build());
+            // required to force order of updates that could break NOMS_NUMBER unique constraint
+            offenderRepository.flush();
+            spgNotificationService.notifyUpdateOfOffender(duplicateOffender);
+        });
+        offender.setNomsNumber(nomsNumber);
+        spgNotificationService.notifyUpdateOfOffender(offender);
     }
 }
--- src/test/java/uk/gov/justice/digital/delius/service/OffenderIdentifierServiceTest.java
+++ src/test/java/uk/gov/justice/digital/delius/service/OffenderIdentifierServiceTest.java
@@ -5,12 +5,16 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import uk.gov.justice.digital.delius.controller.NotFoundException;
+import uk.gov.justice.digital.delius.data.api.IDs;
 import uk.gov.justice.digital.delius.data.api.UpdateOffenderNomsNumber;
 import uk.gov.justice.digital.delius.jpa.standard.entity.Offender;
+import uk.gov.justice.digital.delius.jpa.standard.entity.StandardReference;
 import uk.gov.justice.digital.delius.jpa.standard.repository.OffenderRepository;
 import uk.gov.justice.digital.delius.transformers.ContactTransformer;
 import uk.gov.justice.digital.delius.transformers.OffenderTransformer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -26,12 +31,13 @@ class OffenderIdentifierServiceTest {
     private OffenderRepository offenderRepository = mock(OffenderRepository.class);
     private SpgNotificationService spgNotificationService = mock(SpgNotificationService.class);
     private ArgumentCaptor<Offender> offenderCaptor = ArgumentCaptor.forClass(Offender.class);
+    private ReferenceDataService referenceDataService = mock(ReferenceDataService.class);
 
     @Nested
     class FeatureSwitchedOff {
         @BeforeEach
         void setUp() {
-            service = new OffenderIdentifierService(false, new OffenderTransformer(new ContactTransformer()), offenderRepository, spgNotificationService);
+            service = new OffenderIdentifierService(false, new OffenderTransformer(new ContactTransformer()), offenderRepository, spgNotificationService, referenceDataService);
         }
 
         @Test
@@ -56,93 +62,149 @@ class OffenderIdentifierServiceTest {
     class FeatureSwitchedOn {
         @BeforeEach
         void setUp() {
-            service = new OffenderIdentifierService(true, new OffenderTransformer(new ContactTransformer()), offenderRepository, spgNotificationService);
+            service = new OffenderIdentifierService(true, new OffenderTransformer(new ContactTransformer()), offenderRepository, spgNotificationService, referenceDataService);
+            when(offenderRepository.findByNomsNumber(any())).thenReturn(Optional.empty());
         }
 
-        @Test
-        void willThrowNotFoundWhenOffenderNotFound() {
-            when(offenderRepository.findByCrn("X12345")).thenReturn(Optional.empty());
-            assertThrows(NotFoundException.class, () -> service
-                    .updateNomsNumber("X12345", UpdateOffenderNomsNumber.builder().nomsNumber("G5555TT").build()));
+        @Nested
+        class NoOffenderFound {
+            @Test
+            void willThrowNotFoundWhenOffenderNotFound() {
+                when(offenderRepository.findByCrn("X12345")).thenReturn(Optional.empty());
+                assertThrows(NotFoundException.class, () -> service
+                        .updateNomsNumber("X12345", UpdateOffenderNomsNumber.builder().nomsNumber("G5555TT").build()));
+            }
+
         }
 
-        @Test
-        void willReturnUpdatedIDsWhenAllOK() {
-            when(offenderRepository.findByCrn("X12345")).thenReturn(Optional.of(
-                    Offender
-                            .builder()
-                            .crn("X12345")
-                            .pncNumber("2018/0012345X")
-                            .build()
-            ));
-            final var iDs = service
-                    .updateNomsNumber("X12345", UpdateOffenderNomsNumber.builder().nomsNumber("G5555TT").build());
+        @Nested
+        class NewNOMSNumber {
+            @BeforeEach
+            void setUp() {
+                when(offenderRepository.findAllByNomsNumber(any())).thenReturn(List.of());
+            }
 
-            assertThat(iDs.getCrn()).isEqualTo("X12345");
-            assertThat(iDs.getNomsNumber()).isEqualTo("G5555TT");
-            assertThat(iDs.getPncNumber()).isEqualTo("2018/0012345X");
-        }
+            @Test
+            void willReturnUpdatedIDsWhenAllOK() {
+                when(offenderRepository.findByCrn("X12345")).thenReturn(Optional.of(
+                        Offender
+                                .builder()
+                                .crn("X12345")
+                                .pncNumber("2018/0012345X")
+                                .build()
+                ));
+                final var iDs = service
+                        .updateNomsNumber("X12345", UpdateOffenderNomsNumber.builder().nomsNumber("G5555TT").build());
 
-        @Test
-        void willUpdateOffender() {
-            when(offenderRepository.findByCrn("X12345")).thenReturn(Optional.of(
-                    Offender
-                            .builder()
-                            .crn("X12345")
-                            .pncNumber("2018/0012345X")
-                            .build()
-            ));
-            service.updateNomsNumber("X12345", UpdateOffenderNomsNumber.builder().nomsNumber("G5555TT").build());
+                assertThat(iDs.getCrn()).isEqualTo("X12345");
+                assertThat(iDs.getNomsNumber()).isEqualTo("G5555TT");
+                assertThat(iDs.getPncNumber()).isEqualTo("2018/0012345X");
+            }
 
-            verify(offenderRepository).save(offenderCaptor.capture());
+            @Test
+            void willUpdateOffender() {
+                when(offenderRepository.findByCrn("X12345")).thenReturn(Optional.of(
+                        Offender
+                                .builder()
+                                .crn("X12345")
+                                .pncNumber("2018/0012345X")
+                                .build()
+                ));
+                final var IDs = service.updateNomsNumber("X12345", UpdateOffenderNomsNumber.builder().nomsNumber("G5555TT").build());
 
-            assertThat(offenderCaptor.getValue().getNomsNumber()).isEqualTo("G5555TT");
-        }
+                assertThat(IDs.getNomsNumber()).isEqualTo("G5555TT");
+            }
 
-        @Test
-        void willNotifySPGOfOffenderChanges() {
-            var offender = Offender
-                    .builder()
-                    .crn("X12345")
-                    .pncNumber("2018/0012345X")
-                    .build();
-            when(offenderRepository.findByCrn("X12345")).thenReturn(Optional.of(offender));
-            service.updateNomsNumber("X12345", UpdateOffenderNomsNumber.builder().nomsNumber("G5555TT").build());
+            @Test
+            void willNotifySPGOfOffenderChanges() {
+                var offender = Offender
+                        .builder()
+                        .crn("X12345")
+                        .pncNumber("2018/0012345X")
+                        .build();
+                when(offenderRepository.findByCrn("X12345")).thenReturn(Optional.of(offender));
+                service.updateNomsNumber("X12345", UpdateOffenderNomsNumber.builder().nomsNumber("G5555TT").build());
 
-            verify(spgNotificationService).notifyUpdateOfOffender(offender);
+                verify(spgNotificationService).notifyUpdateOfOffender(offender);
+            }
         }
 
         @Nested
         class NewNOMSNumberAlreadySet {
-            @Test
-            void willNotUpdateOffender() {
-                when(offenderRepository.findByCrn("X12345")).thenReturn(Optional.of(
-                        Offender
-                                .builder()
-                                .crn("X12345")
-                                .pncNumber("2018/0012345X")
-                                .nomsNumber("G5555TT")
-                                .build()
-                ));
-                service.updateNomsNumber("X12345", UpdateOffenderNomsNumber.builder().nomsNumber("G5555TT").build());
-
-                verify(offenderRepository, never()).save(any());
+            @BeforeEach
+            void setUp() {
+                when(offenderRepository.findAllByNomsNumber(any())).thenReturn(List.of());
             }
+
             @Test
             void willNotNotifySPG() {
+                final Offender offender = Offender
+                        .builder()
+                        .crn("X12345")
+                        .pncNumber("2018/0012345X")
+                        .nomsNumber("G5555TT")
+                        .build();
+                when(offenderRepository.findByCrn("X12345")).thenReturn(Optional.of(offender));
+                service.updateNomsNumber("X12345", UpdateOffenderNomsNumber.builder().nomsNumber("G5555TT").build());
+
+                verify(spgNotificationService, never()).notifyUpdateOfOffender(offender);
+            }
+
+        }
+
+        @Nested
+        class NOMSAssignedToOtherOffender {
+            private final Offender duplicateOffender = Offender
+                    .builder()
+                    .offenderId(88L)
+                    .crn("X88888")
+                    .nomsNumber("G5555TT")
+                    .additionalIdentifiers(new ArrayList<>())
+                    .build();
+
+            private IDs iDs;
+
+            @BeforeEach
+            void setUp() {
+                when(offenderRepository.findAllByNomsNumber(any())).thenReturn(List.of(duplicateOffender));
                 when(offenderRepository.findByCrn("X12345")).thenReturn(Optional.of(
                         Offender
                                 .builder()
+                                .offenderId(99L)
                                 .crn("X12345")
-                                .pncNumber("2018/0012345X")
-                                .nomsNumber("G5555TT")
                                 .build()
                 ));
-                service.updateNomsNumber("X12345", UpdateOffenderNomsNumber.builder().nomsNumber("G5555TT").build());
-
-                verify(spgNotificationService, never()).notifyUpdateOfOffender(any());
+                when(referenceDataService.duplicateNomsNumberAdditionalIdentifier()).thenReturn(StandardReference
+                        .builder()
+                        .codeValue("DNOMS")
+                        .codeDescription("Duplicate NOMIS Number")
+                        .build());
+                iDs = service.updateNomsNumber("X12345", UpdateOffenderNomsNumber.builder().nomsNumber("G5555TT").build());
             }
 
+            @Test
+            void willUpdateOffender() {
+                assertThat(iDs.getNomsNumber()).isEqualTo("G5555TT");
+            }
+
+            @Test
+            void willUpdateDuplicateOffender() {
+                verify(spgNotificationService, times(2)).notifyUpdateOfOffender(offenderCaptor.capture());
+
+                // can only test this via spg call since this is a side affect
+                final Offender duplicateOffender = offenderCaptor.getAllValues().get(0);
+                assertThat(duplicateOffender.getOffenderId()).isEqualTo(88L);
+                assertThat(duplicateOffender.getNomsNumber()).isNull();
+                assertThat(duplicateOffender.getAdditionalIdentifiers()).hasSize(1);
+                assertThat(duplicateOffender.getAdditionalIdentifiers().get(0).getIdentifierName().getCodeValue()).isEqualTo("DNOMS");
+            }
+
+            @Test
+            void willNotifySPGForDuplicateOffender() {
+                verify(spgNotificationService, times(2)).notifyUpdateOfOffender(offenderCaptor.capture());
+                final Offender duplicateOffender = offenderCaptor.getAllValues().get(0);
+                assertThat(duplicateOffender.getOffenderId()).isEqualTo(88L);
+            }
         }
     }
 }
