2c59d1f2f77a42bcf8bcbf24b404eba25f94c768
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-05-20 12:25:03 +0100

    DT-835 Convert institutional report controller tests to unit tests

--- src/main/java/uk/gov/justice/digital/delius/data/api/InstitutionalReport.java
+++ src/main/java/uk/gov/justice/digital/delius/data/api/InstitutionalReport.java
@@ -18,6 +18,7 @@ public class InstitutionalReport {
     private Long offenderId;
     private Conviction conviction;
 
+    @JsonIgnore
     @ApiModelProperty(notes = "Deprecated - Use conviction to access sentence")
     public Sentence getSentence() {
         return Optional.ofNullable(conviction).map(Conviction::getSentence).orElse(null);
