cf50ad74eff3098e2fedd507c7bcea6265e2941a
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-05-21 14:39:30 +0100

    DT-835 Unrequired

--- src/main/java/uk/gov/justice/digital/delius/data/api/InstitutionalReport.java
+++ src/main/java/uk/gov/justice/digital/delius/data/api/InstitutionalReport.java
@@ -18,7 +18,6 @@ public class InstitutionalReport {
     private Long offenderId;
     private Conviction conviction;
 
-    @JsonIgnore
     @ApiModelProperty(notes = "Deprecated - Use conviction to access sentence")
     public Sentence getSentence() {
         return Optional.ofNullable(conviction).map(Conviction::getSentence).orElse(null);
