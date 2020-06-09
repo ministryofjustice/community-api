ea6aed23205f1511c244164d0a090515ed8abf86
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-06-09 16:49:12 +0100

    DT-908 Endpoint to replace noms number (for offender merges)

--- src/main/java/uk/gov/justice/digital/delius/controller/secure/CustodyResource.java
+++ src/main/java/uk/gov/justice/digital/delius/controller/secure/CustodyResource.java
@@ -22,6 +22,7 @@ import uk.gov.justice.digital.delius.service.CustodyService;
 import uk.gov.justice.digital.delius.service.OffenderIdentifierService;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -83,5 +84,19 @@ public class CustodyResource {
         return offenderIdentifierService.updateNomsNumber(crn, updateOffenderNomsNumber);
     }
 
+    @RequestMapping(value = "offenders/nomsNumber/{originalNomsNumber}/nomsNumber", method = RequestMethod.PUT, consumes = "application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "The new noms number is not present"),
+            @ApiResponse(code = 401, message = "Request is missing Authorization header (no JWT)"),
+            @ApiResponse(code = 403, message = "Missing required role for this operation"),
+            @ApiResponse(code = 404, message = "The requested offender was not found")
+    })
+    @ApiOperation(value = "Updates the offender record(s) with the new NOMS number in UpdateOffenderNomsNumber replacing the existing number.",
+            notes = "In the very rare circumstances more than one offender is found with matching noms number, all will be updated and their identifiers returned.")
+    public List<IDs> replaceOffenderNomsNumber(final @PathVariable String originalNomsNumber,
+                                               final @RequestBody @Valid UpdateOffenderNomsNumber updateOffenderNomsNumber) {
+        log.info("Call to replaceOffenderNomsNumber for {}", originalNomsNumber);
 
+        return offenderIdentifierService.replaceNomsNumber(originalNomsNumber, updateOffenderNomsNumber);
+    }
 }
---
+++ src/test/java/uk/gov/justice/digital/delius/controller/secure/CustodyController_replaceNomsNumberTest.java
@@ -0,0 +1,139 @@
+package uk.gov.justice.digital.delius.controller.secure;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import uk.gov.justice.digital.delius.controller.advice.SecureControllerAdvice;
+import uk.gov.justice.digital.delius.data.api.IDs;
+import uk.gov.justice.digital.delius.data.api.UpdateOffenderNomsNumber;
+import uk.gov.justice.digital.delius.service.CustodyService;
+import uk.gov.justice.digital.delius.service.OffenderIdentifierService;
+
+import java.util.List;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+public class CustodyController_replaceNomsNumberTest {
+
+    private CustodyService custodyService = mock(CustodyService.class);
+    private OffenderIdentifierService offenderIdentifierService = mock(OffenderIdentifierService.class);
+    private ArgumentCaptor<UpdateOffenderNomsNumber> updateOffenderNomsNumberArgumentCaptor = ArgumentCaptor
+            .forClass(UpdateOffenderNomsNumber.class);
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Before
+    public void setup() {
+        RestAssuredMockMvc.standaloneSetup(
+                new CustodyResource(custodyService, offenderIdentifierService),
+                new SecureControllerAdvice()
+        );
+
+        when(offenderIdentifierService.replaceNomsNumber(anyString(), any()))
+                .thenReturn(List.of(IDs.builder().build()));
+    }
+
+    @Test
+    public void requestMissingNomsNumber_returnsBadRequest() throws JsonProcessingException {
+        given()
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(json(createUpdateOffenderNomsNumberOf(null)))
+                .when()
+                .put("/secure/offenders/nomsNumber/G3333AA/nomsNumber")
+                .then()
+                .statusCode(400)
+                .body("developerMessage", containsString("Missing a NOMS number"));
+    }
+
+    @Test
+    public void requestBlankNomsNumber_returnsBadRequest() throws JsonProcessingException {
+        given()
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(json(createUpdateOffenderNomsNumberOf("")))
+                .when()
+                .put("/secure/offenders/nomsNumber/G3333AA/nomsNumber")
+                .then()
+                .statusCode(400)
+                .body("developerMessage", containsString("Missing a NOMS number"));
+    }
+
+    @Test
+    public void requestAllPresent_returnsOK() throws JsonProcessingException {
+        given()
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(json(createUpdateOffenderNomsNumberOf("G5555TT")))
+                .when()
+                .put("/secure/offenders/nomsNumber/G3333AA/nomsNumber")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void requestReturnsAllUpdatedOffenderIDs() throws JsonProcessingException {
+        when(offenderIdentifierService.replaceNomsNumber(anyString(), any())).thenReturn(List.of(
+                IDs
+                        .builder()
+                        .nomsNumber("G5555TT")
+                        .crn("X12345")
+                        .build(),
+                IDs
+                        .builder()
+                        .nomsNumber("G5555TT")
+                        .crn("X92382")
+                        .build()));
+
+        given()
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(json(createUpdateOffenderNomsNumberOf("G5555TT")))
+                .when()
+                .put("/secure/offenders/nomsNumber/G3333AA/nomsNumber")
+                .then()
+                .statusCode(200)
+                .body("[0].nomsNumber", equalTo("G5555TT"))
+                .body("[0].crn", equalTo("X12345"))
+                .body("[1].nomsNumber", equalTo("G5555TT"))
+                .body("[1].crn", equalTo("X92382"))
+                ;
+    }
+
+    @Test
+    public void requestWillCallUpdateCustodyService() throws JsonProcessingException {
+        given()
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(json(createUpdateOffenderNomsNumberOf("G5555TT")))
+                .when()
+                .put("/secure/offenders/nomsNumber/G3333AA/nomsNumber")
+                .then()
+                .statusCode(200);
+
+
+        verify(offenderIdentifierService)
+                .replaceNomsNumber(eq("G3333AA"), updateOffenderNomsNumberArgumentCaptor.capture());
+        assertThat(updateOffenderNomsNumberArgumentCaptor.getValue().getNomsNumber()).isEqualTo("G5555TT");
+    }
+
+    private UpdateOffenderNomsNumber createUpdateOffenderNomsNumberOf(String nomsNumber) {
+        return UpdateOffenderNomsNumber
+                .builder()
+                .nomsNumber(nomsNumber)
+                .build();
+    }
+
+    private String json(UpdateOffenderNomsNumber custody) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(custody);
+    }
+
+}
