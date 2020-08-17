5c389531ccd3c7f0800dbe4ce9977940f4263204
Tom Myers <tom.myers@digital.justice.gov.uk>
2020-08-17 13:24:05 +0100

    add username field to /users/search and /users/{username}/detail responses

--- src/test/java/uk/gov/justice/digital/delius/service/UserServiceTest.java
+++ src/test/java/uk/gov/justice/digital/delius/service/UserServiceTest.java
@@ -365,6 +365,7 @@ public class UserServiceTest {
                 .roles(List.of(UserRole.builder().name("ROLE1").build()))
                 .enabled(true)
                 .userId(12345L)
+                .username("john.bean")
                 .build());
     }
 
@@ -402,6 +403,7 @@ public class UserServiceTest {
                     .roles(List.of(UserRole.builder().name("ROLE1").build()))
                     .enabled(true)
                     .userId(12345L)
+                    .username("john.bean")
                     .build());
     }
 
