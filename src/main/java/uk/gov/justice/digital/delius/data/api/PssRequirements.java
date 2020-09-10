40fce7bd0d57b20951e063b7cde212d9e7141389
John Evans <john.evans@digital.justice.gov.uk>
2020-09-10 09:29:29 +0100

    📝 Add swagger docs to PssRequirements

--- src/main/java/uk/gov/justice/digital/delius/data/api/PssRequirements.java
+++ src/main/java/uk/gov/justice/digital/delius/data/api/PssRequirements.java
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.delius.data.api;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,5 +16,6 @@ import java.util.List;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PssRequirements {
+    @ApiModelProperty(value = "List of pssRequirements associated with this conviction")
     private List<PssRequirement> pssRequirements;
 }
