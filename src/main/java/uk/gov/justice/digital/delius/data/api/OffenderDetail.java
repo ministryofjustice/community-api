7502e36e87f2beb8f30ce3e94e89ca078032b9c8
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-11-26 08:33:20 +0000

    DT-1354 Improve swagger - particularly for currentDisposal (#311)

--- src/main/java/uk/gov/justice/digital/delius/data/api/OffenderDetail.java
+++ src/main/java/uk/gov/justice/digital/delius/data/api/OffenderDetail.java
@@ -16,27 +16,41 @@ import java.util.List;
 public class OffenderDetail {
     @ApiModelProperty(required = true)
     private Long offenderId;
+    @ApiModelProperty(example = "Mr")
     private String title;
+    @ApiModelProperty(example = "John")
     private String firstName;
     private List<String> middleNames;
+    @ApiModelProperty(example = "Smith")
     private String surname;
+    @ApiModelProperty(example = "Davis")
     private String previousSurname;
+    @ApiModelProperty(example = "1982-10-24")
     private LocalDate dateOfBirth;
+    @ApiModelProperty(example = "Male")
     private String gender;
     private IDs otherIds;
     private ContactDetails contactDetails;
     private OffenderProfile offenderProfile;
     private List<OffenderAlias> offenderAliases;
     private List<OffenderManager> offenderManagers;
+    @ApiModelProperty(value = "When true this record has been deleted")
     private Boolean softDeleted;
+    @ApiModelProperty(value = "deprecated, use activeProbationManagedSentence", example = "1")
     private String currentDisposal;
+    @ApiModelProperty(example = "National Data")
     private String partitionArea;
+    @ApiModelProperty(value = "When true this record can only be viewed by specific probation staff")
     private Boolean currentRestriction;
+    @ApiModelProperty(value = "Message to show staff who have not been included to view this record")
     private String restrictionMessage;
+    @ApiModelProperty(value = "When true this record can not be viewed by specific probation staff")
     private Boolean currentExclusion;
+    @ApiModelProperty(value = "Message to show staff who have been excluded from viewing this record")
     private String exclusionMessage;
     @ApiModelProperty(value = "current tier", example = "D2")
     private String currentTier;
+    @ApiModelProperty(value = "identifies if this person is on an active sentence of interest to probation", notes = "this is an alias of currentDisposal but in Boolean form")
     public boolean isActiveProbationManagedSentence() {
         return "1".equals(currentDisposal);
     }
