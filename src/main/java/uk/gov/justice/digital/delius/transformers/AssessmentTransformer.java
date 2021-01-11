7b914327a87c65e875aa791c885d3432e253b063
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-01-11 12:06:45 +0000

    MAS-226 return assessment with most recent assessment date

--- src/main/java/uk/gov/justice/digital/delius/transformers/AssessmentTransformer.java
+++ src/main/java/uk/gov/justice/digital/delius/transformers/AssessmentTransformer.java
@@ -5,6 +5,8 @@ import uk.gov.justice.digital.delius.jpa.standard.entity.OASYSAssessment;
 import uk.gov.justice.digital.delius.jpa.standard.entity.OGRSAssessment;
 import uk.gov.justice.digital.delius.jpa.standard.entity.Offender;
 
+import java.time.LocalDate;
+
 public class AssessmentTransformer {
 
     public static OffenderAssessments assessmentsOf(Offender offender, OGRSAssessment OGRSAssessment, OASYSAssessment OASYSAssessment) {
@@ -16,14 +18,34 @@ public class AssessmentTransformer {
     }
 
     private static Integer getOGRSScore(OGRSAssessment OGRSAssessment, OASYSAssessment OASYSAssessment) {
+        Integer OGRSAssessmentScore = null;
+        Integer OASYSAssessmentScore = null;
+        LocalDate OGRSAssessmentDate = null;
+        LocalDate OASYSAssessmentDate = null;
+
         if (null != OGRSAssessment) {
-            if(null != OGRSAssessment.getOGRS3Score2()){
-                return OGRSAssessment.getOGRS3Score2();
-            }
+            OGRSAssessmentScore = OGRSAssessment.getOGRS3Score2();
+            OGRSAssessmentDate = OGRSAssessment.getAssessmentDate();
         }
         if (null != OASYSAssessment) {
-            return OASYSAssessment.getOGRSScore2();
+            OASYSAssessmentScore = OASYSAssessment.getOGRSScore2();
+            OASYSAssessmentDate = OASYSAssessment.getAssessmentDate();
         }
+        if(null == OASYSAssessmentScore) {
+            return OGRSAssessmentScore;
+        }
+
+        if (null != OGRSAssessmentScore) {
+            if(OGRSAssessmentDate != null) {
+                if (OGRSAssessmentDate.isAfter(OASYSAssessmentDate)) {
+                    return OGRSAssessmentScore;
+                }
+            }
+        }
+        if (null != OASYSAssessmentScore) {
+            return OASYSAssessmentScore;
+        }
+
         return null;
     }
 
--- src/test/java/uk/gov/justice/digital/delius/transformers/AssessmentTransfomerTest.java
+++ src/test/java/uk/gov/justice/digital/delius/transformers/AssessmentTransfomerTest.java
@@ -5,6 +5,9 @@ import uk.gov.justice.digital.delius.data.api.OffenderAssessments;
 import uk.gov.justice.digital.delius.jpa.standard.entity.OASYSAssessment;
 import uk.gov.justice.digital.delius.jpa.standard.entity.OGRSAssessment;
 import uk.gov.justice.digital.delius.jpa.standard.entity.Offender;
+
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AssessmentTransfomerTest {
@@ -39,6 +42,22 @@ public class AssessmentTransfomerTest {
             .dynamicRsrScore(1.65)
             .build(), OGRSAssessment.builder().build(), OASYSAssessment.builder().OGRSScore2(4).build()))
             .isEqualTo(OffenderAssessments.builder().rsrScore(1.65).OGRSScore(4).build());
+    }
+
+    @Test
+    void bothPresentBothHaveAssessmentOGRSIsMoreRecent() {
+        assertThat(AssessmentTransformer.assessmentsOf(Offender.builder()
+            .dynamicRsrScore(0.12)
+            .build(), OGRSAssessment.builder().assessmentDate(LocalDate.of(2020,1,1)).OGRS3Score2(11).build(), OASYSAssessment.builder().assessmentDate(LocalDate.of(2018,1,1)).OGRSScore2(33).build()))
+            .isEqualTo(OffenderAssessments.builder().rsrScore(0.12).OGRSScore(11).build());
+    }
+
+    @Test
+    void bothPresentBothHaveAssessmentOASYSIsMoreRecent() {
+        assertThat(AssessmentTransformer.assessmentsOf(Offender.builder()
+            .dynamicRsrScore(0.12)
+            .build(), OGRSAssessment.builder().assessmentDate(LocalDate.of(2018,1,1)).OGRS3Score2(11).build(), OASYSAssessment.builder().assessmentDate(LocalDate.of(2020,1,1)).OGRSScore2(33).build()))
+            .isEqualTo(OffenderAssessments.builder().rsrScore(0.12).OGRSScore(33).build());
     }
 
     @Test
