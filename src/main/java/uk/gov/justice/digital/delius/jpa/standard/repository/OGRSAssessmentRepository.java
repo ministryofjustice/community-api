953af24fc3a7d6df010a64a9d4b7094b905eafdf
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-01-12 07:41:45 +0000

    MAS-226 check event has not been deleted when getting OGRS assessment

--- src/main/java/uk/gov/justice/digital/delius/jpa/standard/repository/OGRSAssessmentRepository.java
+++ src/main/java/uk/gov/justice/digital/delius/jpa/standard/repository/OGRSAssessmentRepository.java
@@ -11,12 +11,18 @@ import java.util.Optional;
 @Repository
 public interface OGRSAssessmentRepository extends JpaRepository<OGRSAssessment, Long> {
 
-    @Query("select assessment from OGRSAssessment assessment inner join assessment.event event where event.offenderId = :offenderId and assessment.softDeleted = 0 and assessment.lastUpdatedDate = \n" +
-        "    (select max(assessmentLatest.lastUpdatedDate) \n" +
-        "    from OGRSAssessment assessmentLatest \n" +
-        "    inner join assessmentLatest.event eventLatest \n" +
-        "    where eventLatest.offenderId = :offenderId \n" +
-        "    and assessmentLatest.softDeleted = 0)")
+    @Query("select assessment from OGRSAssessment assessment " +
+        "inner join assessment.event event " +
+        "where event.offenderId = :offenderId " +
+        "and event.softDeleted = 0 " +
+        "and assessment.softDeleted = 0 " +
+        "and assessment.lastUpdatedDate = \n" +
+        "   (select max(assessmentLatest.lastUpdatedDate) \n" +
+        "   from OGRSAssessment assessmentLatest \n" +
+        "   inner join assessmentLatest.event eventLatest \n" +
+        "   where eventLatest.offenderId = :offenderId \n" +
+        "   and eventLatest.softDeleted = 0 " +
+        "   and assessmentLatest.softDeleted = 0)")
     Optional<OGRSAssessment> findLatestByOffenderId(@Param("offenderId") Long offenderId);
 
 }
