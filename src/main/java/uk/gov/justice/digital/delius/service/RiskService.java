308d460b92b24ee72a7a80236dbf141ae90e6807
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-01-27 17:50:01 +0000

    DT-1542 Missing TODO

--- src/main/java/uk/gov/justice/digital/delius/service/RiskService.java
+++ src/main/java/uk/gov/justice/digital/delius/service/RiskService.java
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.delius.service;
 import org.springframework.stereotype.Service;
 import uk.gov.justice.digital.delius.data.api.MappaDetails;
 
+// TODO DT-1542 Implement this service
 @Service
 public class RiskService {
     public MappaDetails getMappaDetails(Long offenderId) {
