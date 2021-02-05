f09183432ce1d581ea4b188587d19ae71eaff389
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-02-05 16:22:57 +0000

    DT-1595 Added helper endpoint for amending names (#359)

--- src/testIntegration/java/uk/gov/justice/digital/delius/controller/secure/SmokeTestHelperAPITest.java
+++ src/testIntegration/java/uk/gov/justice/digital/delius/controller/secure/SmokeTestHelperAPITest.java
@@ -1,10 +1,12 @@
 package uk.gov.justice.digital.delius.controller.secure;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.justice.digital.delius.FlywayRestoreExtension;
+import uk.gov.justice.digital.delius.data.api.OffenderDetail;
 import uk.gov.justice.digital.delius.data.api.ReplaceCustodyKeyDates;
 import uk.gov.justice.digital.delius.data.api.UpdateCustody;
 
@@ -12,39 +14,43 @@ import java.time.LocalDate;
 
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @ExtendWith({SpringExtension.class, FlywayRestoreExtension.class})
-@DisplayName("POST /smoketest/offenders/crn/{crn}/custody/reset")
+@DisplayName("Smoke test update helpers")
 public class SmokeTestHelperAPITest extends IntegrationTestBase {
     private static final String NOMS_NUMBER = "G9542VP";
     private static final String CRN = "X320741";
     private static final long CONVICTION_ID = 2500295345L;
     private static final String PRISON_BOOKING_NUMBER = "V74111";
 
-    @Test
-    @DisplayName("must have `ROLE_SMOKE_TEST` to access this service")
-    public void mustHaveCommunityRole() {
-        final var token = createJwt("ROLE_COMMUNITY");
+    @Nested
+    @DisplayName("POST smoketest/offenders/crn/{crn}/custody/reset")
+    class CustodyReset {
+        @Test
+        @DisplayName("must have `ROLE_SMOKE_TEST` to access this service")
+        public void mustHaveCommunityRole() {
+            final var token = createJwt("ROLE_COMMUNITY");
 
-        given()
+            given()
                 .auth().oauth2(token)
                 .contentType(APPLICATION_JSON_VALUE)
                 .when()
                 .post("smoketest/offenders/crn/{crn}/custody/reset", CRN)
                 .then()
                 .statusCode(403);
-    }
+        }
 
-    @Test
-    @DisplayName("Can reset custody data")
-    public void canGetNextUpdateWithDateChanged() {
-        final var token = createJwt("ROLE_COMMUNITY_CUSTODY_UPDATE", "ROLE_COMMUNITY", "ROLE_SMOKE_TEST");
+        @Test
+        @DisplayName("Can reset custody data")
+        public void canResetCustodyData() {
+            final var token = createJwt("ROLE_COMMUNITY_CUSTODY_UPDATE", "ROLE_COMMUNITY", "ROLE_SMOKE_TEST");
 
-        // Given current location is HMP Moorland
-        given()
+            // Given current location is HMP Moorland
+            given()
                 .auth().oauth2(token)
                 .contentType("application/json")
                 .body(createUpdateCustody("MDI"))
@@ -53,27 +59,27 @@ public class SmokeTestHelperAPITest extends IntegrationTestBase {
                 .then()
                 .statusCode(200);
 
-        // AND the conviction has some keys dates
-        given()
+            // AND the conviction has some keys dates
+            given()
                 .auth().oauth2(token)
                 .contentType("application/json")
                 .body(createReplaceCustodyKeyDates(ReplaceCustodyKeyDates
-                        .builder()
-                        .conditionalReleaseDate(LocalDate.now())
-                        .licenceExpiryDate(LocalDate.now())
-                        .hdcEligibilityDate(LocalDate.now())
-                        .paroleEligibilityDate(LocalDate.now())
-                        .sentenceExpiryDate(LocalDate.now())
-                        .expectedReleaseDate(LocalDate.now())
-                        .postSentenceSupervisionEndDate(LocalDate.now())
-                        .build()))
+                    .builder()
+                    .conditionalReleaseDate(LocalDate.now())
+                    .licenceExpiryDate(LocalDate.now())
+                    .hdcEligibilityDate(LocalDate.now())
+                    .paroleEligibilityDate(LocalDate.now())
+                    .sentenceExpiryDate(LocalDate.now())
+                    .expectedReleaseDate(LocalDate.now())
+                    .postSentenceSupervisionEndDate(LocalDate.now())
+                    .build()))
                 .when()
                 .post(String.format("offenders/nomsNumber/%s/bookingNumber/%s/custody/keyDates", NOMS_NUMBER, PRISON_BOOKING_NUMBER))
                 .then()
                 .statusCode(200);
 
-        // AND I verify that data has been set
-        given()
+            // AND I verify that data has been set
+            given()
                 .auth().oauth2(token)
                 .contentType("application/json")
                 .when()
@@ -85,11 +91,11 @@ public class SmokeTestHelperAPITest extends IntegrationTestBase {
                 .body("status.code", equalTo("D"))
                 .body("status.description", equalTo("In Custody"))
                 .body("keyDates.size()", equalTo(7))
-        ;
+            ;
 
 
-        // WHEN when I reset this data
-        given()
+            // WHEN when I reset this data
+            given()
                 .auth().oauth2(token)
                 .contentType(APPLICATION_JSON_VALUE)
                 .when()
@@ -98,8 +104,8 @@ public class SmokeTestHelperAPITest extends IntegrationTestBase {
                 .statusCode(200);
 
 
-        // THEN the custody data is set to initial defaults
-        given()
+            // THEN the custody data is set to initial defaults
+            given()
                 .auth().oauth2(token)
                 .contentType("application/json")
                 .when()
@@ -111,8 +117,8 @@ public class SmokeTestHelperAPITest extends IntegrationTestBase {
                 .body("status.code", equalTo("A"))
                 .body("keyDates.size()", equalTo(0));
 
-        // AND the offender no longer has a NOMS number
-        given()
+            // AND the offender no longer has a NOMS number
+            given()
                 .auth()
                 .oauth2(tokenWithRoleCommunity())
                 .contentType(APPLICATION_JSON_VALUE)
@@ -121,25 +127,101 @@ public class SmokeTestHelperAPITest extends IntegrationTestBase {
                 .then()
                 .statusCode(404);
 
-        // AND I can reset again with no errors
-        given()
+            // AND I can reset again with no errors
+            given()
                 .auth().oauth2(token)
                 .contentType(APPLICATION_JSON_VALUE)
                 .when()
                 .post("/smoketest/offenders/crn/{crn}/custody/reset", CRN)
                 .then()
                 .statusCode(200);
-    }
+        }
 
-    private String createUpdateCustody(@SuppressWarnings("SameParameterValue") String prisonCode) {
-        return writeValueAsString(UpdateCustody
+        private String createUpdateCustody(@SuppressWarnings("SameParameterValue") String prisonCode) {
+            return writeValueAsString(UpdateCustody
                 .builder()
                 .nomsPrisonInstitutionCode(prisonCode)
                 .build());
-    }
+        }
 
-    private String createReplaceCustodyKeyDates(ReplaceCustodyKeyDates replaceCustodyKeyDates) {
-        return writeValueAsString(replaceCustodyKeyDates);
+        private String createReplaceCustodyKeyDates(ReplaceCustodyKeyDates replaceCustodyKeyDates) {
+            return writeValueAsString(replaceCustodyKeyDates);
+        }
     }
+    @Nested
+    @DisplayName("POST smoketest/offenders/crn/{crn}/details")
+    class UpdateOffenderDetails {
+        @Test
+        @DisplayName("must have `ROLE_SMOKE_TEST` to access this service")
+        public void mustHaveCommunityRole() {
+            final var token = createJwt("ROLE_COMMUNITY");
 
+            given()
+                .auth().oauth2(token)
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(createOffenderDetails("Betty", "Boop"))
+                .when()
+                .post("/smoketest/offenders/crn/{crn}/details", CRN)
+                .then()
+                .statusCode(403);
+        }
+
+        @Test
+        @DisplayName("will reject updates with no names")
+        public void mustHaveNamesInBody() {
+            final var token = createJwt("ROLE_SMOKE_TEST");
+
+            given()
+                .auth().oauth2(token)
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(createOffenderDetails("", ""))
+                .when()
+                .post("/smoketest/offenders/crn/{crn}/details", CRN)
+                .then()
+                .statusCode(400);
+        }
+
+        @Test
+        @DisplayName("Can set first name and surname")
+        public void canUpdateOffenderDetails() {
+            final var token = createJwt("ROLE_SMOKE_TEST");
+
+            // GIVEN I change the name
+            given()
+                .auth().oauth2(token)
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(createOffenderDetails("Betty", "Boop"))
+                .when()
+                .post("smoketest/offenders/crn/{crn}/details", CRN)
+                .then()
+                .statusCode(200);
+
+
+            // WHEN I the retrieve the details
+            final var offenderDetail = given()
+                .auth()
+                .oauth2(tokenWithRoleCommunity())
+                .contentType(APPLICATION_JSON_VALUE)
+                .when()
+                .get("/offenders/crn/{crn}/all", CRN)
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(OffenderDetail.class);
+
+            // THEN the name would reflect the name previously saved
+            assertThat(offenderDetail.getFirstName()).isEqualTo("Betty");
+            assertThat(offenderDetail.getSurname()).isEqualTo("Boop");
+
+        }
+
+        private String createOffenderDetails(@SuppressWarnings("SameParameterValue") String firstName, @SuppressWarnings("SameParameterValue") String surname) {
+            return writeValueAsString(uk.gov.justice.digital.delius.data.api.UpdateOffenderDetails
+                .builder()
+                .firstName(firstName)
+                .surname(surname)
+                .build());
+        }
+    }
 }
