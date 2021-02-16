6ab53459434ad9fa44fd8ebf76a7dc4b02e368e1
John Evans <john.evans@digital.justice.gov.uk>
2021-02-16 16:58:30 +0000

    🚧 PIC-1092: Add probationStatus endpoint and Swagger docs

--- src/main/java/uk/gov/justice/digital/delius/data/api/ProbationStatusDetail.java
+++ src/main/java/uk/gov/justice/digital/delius/data/api/ProbationStatusDetail.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.delius.data.api;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,7 +10,10 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class ProbationStatusDetail {
     private final ProbationStatus probationStatus;
+    @ApiModelProperty(value = "The termination date of the most recently terminated sentence")
     private final LocalDate previouslyKnownTerminationDate;
+    @ApiModelProperty(value = "True if the offender is in breach of a current sentence")
     private final Boolean inBreach;
+    @ApiModelProperty(value = "True if the offender has a conviction with no sentence")
     private final Boolean preSentenceActivity;
 }
