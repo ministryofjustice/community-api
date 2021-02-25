1a6de9531d78ad297087f7f738da3e82d8c814e4
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-02-25 18:54:56 +0000

    MAS-317 correct ID type on repository

--- src/main/java/uk/gov/justice/digital/delius/jpa/standard/repository/ManagementTierRepository.java
+++ src/main/java/uk/gov/justice/digital/delius/jpa/standard/repository/ManagementTierRepository.java
@@ -1,12 +1,10 @@
 package uk.gov.justice.digital.delius.jpa.standard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.gov.justice.digital.delius.jpa.standard.entity.ManagementTier;
-
-import java.util.Optional;
+import uk.gov.justice.digital.delius.jpa.standard.entity.ManagementTierId;
 
 @Repository
-public interface ManagementTierRepository extends JpaRepository<ManagementTier, Long> {
+public interface ManagementTierRepository extends JpaRepository<ManagementTier, ManagementTierId> {
 }
