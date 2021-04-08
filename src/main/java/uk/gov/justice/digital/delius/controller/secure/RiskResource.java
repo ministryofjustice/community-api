2d194ad2cb4385dcda8b71c17e49718aa0db53cf
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-04-08 17:40:55 +0100

    DT-1570 Resourcing risk allocation endpoint so that MPC can allocated POMs based on legacy resourcing codes (CRC/NPS split) (#395)

--- src/main/java/uk/gov/justice/digital/delius/controller/secure/RiskResource.java
+++ src/main/java/uk/gov/justice/digital/delius/controller/secure/RiskResource.java
@@ -12,16 +12,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.justice.digital.delius.controller.ConflictingRequestException;
 import uk.gov.justice.digital.delius.controller.NotFoundException;
 import uk.gov.justice.digital.delius.controller.advice.ErrorResponse;
 import uk.gov.justice.digital.delius.data.api.MappaDetails;
-import uk.gov.justice.digital.delius.data.api.OffenderDetail;
+import uk.gov.justice.digital.delius.data.api.RiskResourcingDetails;
 import uk.gov.justice.digital.delius.service.OffenderService;
 import uk.gov.justice.digital.delius.service.RiskService;
 
 import java.util.Optional;
 
-// TODO - DT-1542 Remove BETA from the API description once the service has been fully implemented
 @Api(tags = "Risks and Registrations")
 @RestController
 @Slf4j
@@ -33,7 +33,7 @@ public class RiskResource {
     private final OffenderService offenderService;
 
     @ApiOperation(
-        value = "*** BETA - coming soon *** Return the MAPPA details for an offender using NOMS number", notes = "requires ROLE_COMMUNITY")
+        value = "Return the MAPPA details for an offender using NOMS number", notes = "requires ROLE_COMMUNITY")
     @ApiResponses(
         value = {
             @ApiResponse(code = 400, message = "Invalid request", response = ErrorResponse.class),
@@ -43,11 +43,15 @@ public class RiskResource {
         })
     @GetMapping(value = "offenders/nomsNumber/{nomsNumber}/risk/mappa")
     public MappaDetails getOffenderMappaDetailsByNomsNumber(final @PathVariable("nomsNumber") String nomsNumber) {
-        return mappaDetailsFor(offenderService.offenderIdOfNomsNumber(nomsNumber));
+        final var mayBeOffenderId = offenderService
+            .mostLikelyOffenderIdOfNomsNumber(nomsNumber)
+            .getOrElseThrow(error -> new ConflictingRequestException(error.getMessage()));
+
+        return mappaDetailsFor(mayBeOffenderId);
     }
 
     @ApiOperation(
-        value = "*** BETA - coming soon *** Return the MAPPA details for an offender using CRN", notes = "requires ROLE_COMMUNITY")
+        value = "Return the MAPPA details for an offender using CRN", notes = "requires ROLE_COMMUNITY")
     @ApiResponses(
         value = {
             @ApiResponse(code = 400, message = "Invalid request", response = ErrorResponse.class),
@@ -56,13 +60,52 @@ public class RiskResource {
             @ApiResponse(code = 500, message = "Unrecoverable error whilst processing request.", response = ErrorResponse.class)
         })
     @GetMapping(value = "offenders/crn/{crn}/risk/mappa")
-    public MappaDetails getOffenderMappaDetailsByOffenderId(final @PathVariable("crn") String crn) {
+    public MappaDetails getOffenderMappaDetailsByCrn(final @PathVariable("crn") String crn) {
         return mappaDetailsFor(offenderService.offenderIdOfCrn(crn));
     }
+
+    @ApiOperation(
+        value = "Return the resourcing details for an offender using NOMS number. Typically this is allocated or retained requiring greater resourcing. This equates to the historic NPS/CRC split for low and high risk offenders", notes = "requires ROLE_COMMUNITY")
+    @ApiResponses(
+        value = {
+            @ApiResponse(code = 400, message = "Invalid request", response = ErrorResponse.class),
+            @ApiResponse(code = 403, message = "Forbidden - requires ROLE_COMMUNITY", response = ErrorResponse.class),
+            @ApiResponse(code = 404, message = "Offender not found", response = ErrorResponse.class),
+            @ApiResponse(code = 500, message = "Unrecoverable error whilst processing request.", response = ErrorResponse.class)
+        })
+    @GetMapping(value = "offenders/nomsNumber/{nomsNumber}/risk/resourcing/latest")
+    public RiskResourcingDetails getOffenderResourcingDetailsByNomsNumber(final @PathVariable("nomsNumber") String nomsNumber) {
+        final var mayBeOffenderId = offenderService
+            .mostLikelyOffenderIdOfNomsNumber(nomsNumber)
+            .getOrElseThrow(error -> new ConflictingRequestException(error.getMessage()));
+
+        return resourcingDetailsFor(mayBeOffenderId);
+    }
+
+    @ApiOperation(
+        value = "Return the resourcing details for an offender using CRN number. Typically this is allocated or retained requiring greater resourcing. This equates to the historic NPS/CRC split for low and high risk offenders", notes = "requires ROLE_COMMUNITY")
+    @ApiResponses(
+        value = {
+            @ApiResponse(code = 400, message = "Invalid request", response = ErrorResponse.class),
+            @ApiResponse(code = 403, message = "Forbidden - requires ROLE_COMMUNITY", response = ErrorResponse.class),
+            @ApiResponse(code = 404, message = "Offender not found", response = ErrorResponse.class),
+            @ApiResponse(code = 500, message = "Unrecoverable error whilst processing request.", response = ErrorResponse.class)
+        })
+    @GetMapping(value = "offenders/crn/{crn}/risk/resourcing/latest")
+    public RiskResourcingDetails getOffenderResourcingDetailsByCrn(final @PathVariable("crn") String crn) {
+        return resourcingDetailsFor(offenderService.offenderIdOfCrn(crn));
+    }
+
 
     private MappaDetails mappaDetailsFor(final Optional<Long> maybeOffenderId) {
         return maybeOffenderId
             .map(riskService::getMappaDetails)
             .orElseThrow(() -> new NotFoundException("Offender not found"));
+    }
+    private RiskResourcingDetails resourcingDetailsFor(final Optional<Long> maybeOffenderId) {
+        return maybeOffenderId
+            .map(riskService::getResourcingDetails)
+            .orElseThrow(() -> new NotFoundException("Offender not found"))
+            .orElseThrow(() -> new NotFoundException("Resourcing details for offender not found"));
     }
 }
---
+++ src/test/java/uk/gov/justice/digital/delius/service/RiskServiceTest.java
@@ -0,0 +1,71 @@
+package uk.gov.justice.digital.delius.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.justice.digital.delius.jpa.standard.entity.CaseAllocation;
+import uk.gov.justice.digital.delius.jpa.standard.entity.Event;
+import uk.gov.justice.digital.delius.jpa.standard.entity.StandardReference;
+import uk.gov.justice.digital.delius.jpa.standard.repository.CaseAllocationRepository;
+import uk.gov.justice.digital.delius.jpa.standard.repository.RegistrationRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class RiskServiceTest {
+    @Mock
+    private CaseAllocationRepository caseAllocationRepository;
+    @Mock
+    private RegistrationRepository registrationRepository;
+
+    private RiskService riskService;
+
+    @BeforeEach
+    void setUp() {
+        riskService = new RiskService(registrationRepository, caseAllocationRepository);
+    }
+
+    @Nested
+    @DisplayName("GetResourcingDetails")
+    class GetResourcingDetails {
+        @Test
+        @DisplayName("will return resourcing details when found")
+        void willReturnResourcingDetailsWhenFound() {
+            when(caseAllocationRepository.findFirstByOffenderIdOrderByAllocationDecisionDateDesc(99L))
+                .thenReturn(Optional
+                    .of(CaseAllocation
+                        .builder()
+                        .allocationDecision(StandardReference
+                            .builder()
+                            .codeValue("R")
+                            .codeDescription("Retained")
+                            .build())
+                        .allocationDecisionDate(LocalDateTime.parse("2020-07-19T10:00:00"))
+                        .event(Event.builder().eventId(88L).build())
+                        .build()));
+
+            final var resourcingDetails = riskService.getResourcingDetails(99L);
+
+            assertThat(resourcingDetails).isPresent();
+        }
+
+        @Test
+        @DisplayName("will return empty when not found")
+        void willReturnEmptyWhenNotFound() {
+            when(caseAllocationRepository.findFirstByOffenderIdOrderByAllocationDecisionDateDesc(99L))
+                .thenReturn(Optional.empty());
+            final var resourcingDetails = riskService.getResourcingDetails(99L);
+
+            assertThat(resourcingDetails).isEmpty();
+        }
+
+    }
+}
