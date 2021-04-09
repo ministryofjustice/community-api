76b74446b906f4ce51433fe619b187676ae558e8
markreesmoj <76954782+markreesmoj@users.noreply.github.com>
2021-04-09 08:15:17 +0100

    MAS-393 optionally return active requirements only (#396)
    * MAS-393 optionally filter requirements
    
    * MAS-393 refactoring

--- src/testIntegration/java/uk/gov/justice/digital/delius/controller/secure/RequirementsResource_getRequirements.java
+++ src/testIntegration/java/uk/gov/justice/digital/delius/controller/secure/RequirementsResource_getRequirements.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 public class RequirementsResource_getRequirements extends IntegrationTestBase {
@@ -25,5 +26,36 @@ public class RequirementsResource_getRequirements extends IntegrationTestBase {
                 .statusCode(200)
                 .body("requirements[0].restrictive", equalTo( false));
     }
+
+    @Test
+    public void canRetrieveAllRequirements() {
+
+        given()
+            .auth()
+            .oauth2(tokenWithRoleCommunity())
+            .contentType(APPLICATION_JSON_VALUE)
+            .when()
+            .get(String.format(REQUIREMENTS_PATH, KNOWN_OFFENDER_CRN, KNOWN_CONVICTION_ID))
+            .then()
+            .statusCode(200)
+            .body("requirements", hasSize(2));
+
+    }
+    @Test
+    public void canRetrieveActiveRequirements() {
+
+        given()
+            .auth()
+            .oauth2(tokenWithRoleCommunity())
+            .contentType(APPLICATION_JSON_VALUE)
+            .when()
+            .get(String.format(REQUIREMENTS_PATH, KNOWN_OFFENDER_CRN, KNOWN_CONVICTION_ID) + "?activeOnly=true")
+            .then()
+            .statusCode(200)
+            .body("requirements", hasSize(1));
+
+    }
+
+
 
 }
