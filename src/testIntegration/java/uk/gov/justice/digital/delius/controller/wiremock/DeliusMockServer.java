2eb002a5d7367d083af2104f95ce49bf10e362c1
petergphillips <peter@greenthistle.co.uk>
2021-05-11 13:51:43 +0100

    DT-2010: 🐛 Handle 500 errors from delius for omic opd notes (#430)

--- src/testIntegration/java/uk/gov/justice/digital/delius/controller/wiremock/DeliusMockServer.java
+++ src/testIntegration/java/uk/gov/justice/digital/delius/controller/wiremock/DeliusMockServer.java
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.delius.controller.wiremock;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.springframework.http.HttpStatus;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.put;
@@ -16,28 +17,21 @@ public class DeliusMockServer extends WireMockServer {
     public DeliusMockServer(final int port, final String fileDirectory) {
         super(WireMockConfiguration.wireMockConfig().port(port).usingFilesUnderDirectory(fileDirectory).jettyStopTimeout(10000L));
     }
+
     public void stubPutCaseNoteToDeliusCreated(final String nomisId, final Long caseNotesId) {
-        final String putCaseNote = "/nomisCaseNotes/" + nomisId + "/" + caseNotesId;
+        final var putCaseNote = "/nomisCaseNotes/" + nomisId + "/" + caseNotesId;
         stubFor(put(urlPathMatching(putCaseNote)).willReturn(aResponse()
-                .withHeader("Content-Type", "application/json")
-                .withStatus(201)
-                .withBody(" XXXX (crn) had a Contact created.")
+            .withHeader("Content-Type", "application/json")
+            .withStatus(201)
+            .withBody(" XXXX (crn) had a Contact created.")
         ));
     }
 
-    public void stubPutCaseNoteToDeliusNoContent(final String nomisId, final Long caseNotesId) {
-        final String putCaseNote = "/nomisCaseNotes/" + nomisId + "/" + caseNotesId;
+    public void stubPutCaseNoteToDeliusError(final String nomisId, final long caseNotesId, final HttpStatus status) {
+        final var putCaseNote = "/nomisCaseNotes/" + nomisId + "/" + caseNotesId;
         stubFor(put(urlPathMatching(putCaseNote)).willReturn(aResponse()
-                .withHeader("Content-Type", "application/json")
-                .withStatus(204)
-        ));
-    }
-
-    public void stubPutCaseNoteToDeliusBadRequestError(final String nomisId, final long caseNotesId) {
-        final String putCaseNote = "/nomisCaseNotes/" + nomisId + "/" + caseNotesId;
-        stubFor(put(urlPathMatching(putCaseNote)).willReturn(aResponse()
-                .withHeader("Content-Type", "application/json")
-                .withStatus(400)
+            .withHeader("Content-Type", "application/json")
+            .withStatus(status.value())
         ));
     }
 }
