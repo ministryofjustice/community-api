7373c6154fc12924ab60ac1ce15c69bafb807ea2
mahalrmoj <78814787+mahalrmoj@users.noreply.github.com>
2021-05-14 15:24:54 +0100

    DAPI-99 Referral End request (#438)

---
+++ src/main/java/uk/gov/justice/digital/delius/data/api/ContextlessReferralEndRequest.java
@@ -0,0 +1,46 @@
+package uk.gov.justice.digital.delius.data.api;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.time.OffsetDateTime;
+
+@Data
+@With
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContextlessReferralEndRequest {
+
+    // Fields used to identify associated Referral/Nsi
+    @NotNull
+    @ApiModelProperty(required = true, value = "Denotes a group of services delivered through a referral to a service user, e.g. Personal Well Being", example = "PWB")
+    private String contractType;
+
+    @NotNull
+    @ApiModelProperty(required = true)
+    private OffsetDateTime startedAt;
+
+    // Fields used for ending the referral
+    @NotNull
+    @ApiModelProperty(required = true)
+    private OffsetDateTime endedAt;
+
+    @Positive
+    @NotNull
+    @ApiModelProperty(required = true)
+    private Long sentenceId;
+
+    @NotNull
+    @ApiModelProperty(required = true)
+    private String endType;
+
+    @NotNull
+    private String notes;
+}
