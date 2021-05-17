67bd6013645f6f70866f5c8519c3bd5c3e7634a3
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2021-05-17 14:12:16 +0100

    reverted formatting changes

--- src/main/java/uk/gov/justice/digital/delius/jpa/standard/entity/Nsi.java
+++ src/main/java/uk/gov/justice/digital/delius/jpa/standard/entity/Nsi.java
@@ -5,14 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
