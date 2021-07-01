e8ba97151dee794605b5996a71dd13bc35857176
Alex Haslehurst <alex.haslehurst@gmail.com>
2021-07-01 09:33:08 +0100

    E2E-606: get offender personal contacts api (#497)
    * E2E-606: get offender personal contacts api
    
    * added missing api docs

---
+++ src/main/java/uk/gov/justice/digital/delius/transformers/PersonalContactTransformer.java
@@ -0,0 +1,29 @@
+package uk.gov.justice.digital.delius.transformers;
+
+import uk.gov.justice.digital.delius.data.api.PersonalContact;
+import uk.gov.justice.digital.delius.jpa.standard.entity.StandardReference;
+
+import java.util.Optional;
+
+public class PersonalContactTransformer {
+    public static PersonalContact personalContactOf(uk.gov.justice.digital.delius.jpa.standard.entity.PersonalContact personalContact) {
+        return PersonalContact.builder()
+            .personalContactId(personalContact.getPersonalContactId())
+            .relationship(personalContact.getRelationship())
+            .startDate(personalContact.getStartDate())
+            .endDate(personalContact.getEndDate())
+            .title(Optional.ofNullable(personalContact.getTitle()).map(StandardReference::getCodeDescription).orElse(null))
+            .firstName(personalContact.getFirstName())
+            .otherNames(personalContact.getOtherNames())
+            .surname(personalContact.getSurname())
+            .previousSurname(personalContact.getPreviousSurname())
+            .mobileNumber(personalContact.getMobileNumber())
+            .emailAddress(personalContact.getEmailAddress())
+            .notes(personalContact.getNotes())
+            .gender(Optional.ofNullable(personalContact.getGender()).map(StandardReference::getCodeDescription).orElse(null))
+            .relationshipType(KeyValueTransformer.keyValueOf(personalContact.getRelationshipType()))
+            .createdDatetime(personalContact.getCreatedDatetime())
+            .lastUpdatedDatetime(personalContact.getLastUpdatedDatetime())
+            .build();
+    }
+}
