971d4a0097c79d316bed729a42149c9f04aee367
Ed Brindley <ed@maidavale.org>
2021-07-22 17:21:28 +0100

    Work on contact type endpoint integration tests

--- src/testIntegration/java/uk/gov/justice/digital/delius/controller/secure/ContactMetaControllerApiTest.java
+++ src/testIntegration/java/uk/gov/justice/digital/delius/controller/secure/ContactMetaControllerApiTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.greaterThan;
+import static io.restassured.RestAssured.withArgs;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class ContactMetaControllerApiTest extends IntegrationTestBase {
@@ -13,6 +14,8 @@ public class ContactMetaControllerApiTest extends IntegrationTestBase {
     public void getAllContactTypes(){
         final var token = createJwt("ROLE_COMMUNITY");
 
+        final var officeVisit = withArgs("COAP");
+
         given()
             .auth().oauth2(token)
             .when()
@@ -20,25 +23,18 @@ public class ContactMetaControllerApiTest extends IntegrationTestBase {
             .then()
             .assertThat()
             .statusCode(HttpStatus.OK.value())
-            .body("size()", greaterThan(0));
-//            .root("find { it.contactType == '%s' }")
-//            .body("description", alcohol, equalTo("Citizenship Alcohol Session (NS)"))
-//            .body("requiresLocation", alcohol, equalTo("REQUIRED"))
-//            .body("orderTypes", alcohol, equalTo(List.of("CJA")))
-//            .body("description", homeVisit, equalTo("Home Visit to Case (NS)"))
-//            .body("requiresLocation", homeVisit, equalTo("NOT_REQUIRED"))
-//            .body("orderTypes", homeVisit, equalTo(List.of("CJA", "LEGACY")))
-//            .body("description", other, equalTo("Other Appointment (Non NS)"))
-//            .body("requiresLocation", other, equalTo("OPTIONAL"))
-//            .body("orderTypes", other, equalTo(List.of("CJA", "LEGACY")))
-//            .body("orderTypes", polygraph, equalTo(List.of()));
-
-
+            .body("size()", equalTo(929))
+            .root("find { it.code == '%s' }")
+            .body("description", officeVisit, equalTo("Planned Office Visit (NS)"))
+            .body("appointment", officeVisit, equalTo(true));
     }
 
     @Test
     public void getContactTypesForACategory(){
         final var token = createJwt("ROLE_COMMUNITY");
+
+        final var phoneContact = withArgs("CTOB");
+        final var emailContact = withArgs("CMOB");
 
         given()
             .auth().oauth2(token)
@@ -48,18 +44,12 @@ public class ContactMetaControllerApiTest extends IntegrationTestBase {
             .then()
             .assertThat()
             .statusCode(HttpStatus.OK.value())
-            .body("size()", greaterThan(0));
-//            .root("find { it.contactType == '%s' }")
-//            .body("description", alcohol, equalTo("Citizenship Alcohol Session (NS)"))
-//            .body("requiresLocation", alcohol, equalTo("REQUIRED"))
-//            .body("orderTypes", alcohol, equalTo(List.of("CJA")))
-//            .body("description", homeVisit, equalTo("Home Visit to Case (NS)"))
-//            .body("requiresLocation", homeVisit, equalTo("NOT_REQUIRED"))
-//            .body("orderTypes", homeVisit, equalTo(List.of("CJA", "LEGACY")))
-//            .body("description", other, equalTo("Other Appointment (Non NS)"))
-//            .body("requiresLocation", other, equalTo("OPTIONAL"))
-//            .body("orderTypes", other, equalTo(List.of("CJA", "LEGACY")))
-//            .body("orderTypes", polygraph, equalTo(List.of()));
+            .body("size()", equalTo(43))
+            .root("find { it.contactType == '%s' }")
+            .body("description", phoneContact, equalTo("Phone Contact to Offender"))
+            .body("appointment", phoneContact, equalTo(false))
+            .body("description", emailContact, equalTo("eMail/Text to Offender"))
+            .body("appointment", emailContact, equalTo(false));
 
 
     }
