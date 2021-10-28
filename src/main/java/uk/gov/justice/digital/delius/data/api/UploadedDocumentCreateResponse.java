ee4507c3ec07e2041ef917bc757871dc3b1d419f
adelaidaa <adelaida.aj@gmail.com>
2021-10-28 17:19:58 +0100

    create document service to create contact and document in delius

---
+++ src/main/java/uk/gov/justice/digital/delius/data/api/UploadedDocumentCreateResponse.java
@@ -0,0 +1,33 @@
+package uk.gov.justice.digital.delius.data.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppointmentCreateResponse {
+
+    @NotNull
+    private Long appointmentId;
+
+    @NotNull
+    private OffsetDateTime appointmentStart;
+
+    @NotNull
+    private OffsetDateTime appointmentEnd;
+
+    @NotNull
+    private String type;
+
+    @NotNull
+    private String typeDescription;
+
+    private Boolean sensitive;
+}
