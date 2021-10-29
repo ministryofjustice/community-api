7accfc05c32cd83e9233ff72d2d409c28211552b
nataliewood <natalie.wood@digital.justice.gov.uk>
2021-10-29 09:44:27 +0100

    remove unused import

--- src/test/java/uk/gov/justice/digital/delius/service/DeliusDocumentsServiceTest.java
+++ src/test/java/uk/gov/justice/digital/delius/service/DeliusDocumentsServiceTest.java
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.delius.service;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
