bba04900b62b067191bd73e62b6e61bab48ea9d8
nataliewood <natalie.wood@digital.justice.gov.uk>
2021-10-29 16:52:02 +0100

    test controller and multipart form upload to delius

--- src/testIntegration/java/uk/gov/justice/digital/delius/controller/wiremock/DeliusApiMockServer.java
+++ src/testIntegration/java/uk/gov/justice/digital/delius/controller/wiremock/DeliusApiMockServer.java
@@ -50,30 +50,32 @@ public class DeliusApiMockServer extends WireMockServer {
     }
 
     public void stubPostNewDocumentToDeliusApi() {
-        stubFor(post(urlPathMatching("/v1/offenders/X12234/contacts/17666633/documents"))
+        stubFor(post(urlPathMatching("/v1/offenders/X320741/contacts/17666633/documents"))
             .willReturn(aResponse()
             .withHeader("Content-Type", "application/json")
             .withStatus(201)
-            .withBody("{" +
-                "  \"author\": \"Author name\"," +
-                "  \"authorProvider\": \"Author Provider\"," +
-                "  \"createdProvider\": \"Created Provider\"," +
-                "  \"creationDate\": \"2021-10-28T13:50:36.787Z\"," +
-                "  \"crn\": \"X12234\"," +
-                "  \"dateLastModified\": \"2021-10-28T13:50:36.787Z\"," +
-                "  \"docRenamed\": true," +
-                "  \"documentName\": \"upwDocument.pdf\"," +
-                "  \"documentType\": \"DOCUMENT\"," +
-                "  \"entityId\": 12882882," +
-                "  \"entityType\": \"CONTACT\"," +
-                "  \"id\": 272732," +
-                "  \"lastModifiedUser\": \"last user\"," +
-                "  \"lockOwner\": \"Firstname Lastname\"," +
-                "  \"locked\": true," +
-                "  \"remoteDocumentId\": \"bc1cea0f-93bd-44f2-8f35-16859da49e71\"," +
-                "  \"reservationOwner\": \"owner lastname\"," +
-                "  \"reserved\": true" +
-                "}")
+            .withBody("""
+                {
+                    "author": "Author name",
+                    "authorProvider": "Author Provider",
+                    "createdProvider": "Created Provider",
+                    "creationDate": "2021-10-28T13:50:36.787Z",
+                    "crn": "X320741",
+                    "dateLastModified": "2021-10-28T13:50:36.787Z",
+                    "docRenamed": true,
+                    "documentName": "upwDocument.pdf",
+                    "documentType": "DOCUMENT",
+                    "entityId": 12882882,
+                    "entityType": "CONTACT",
+                    "id": 272732,
+                    "lastModifiedUser": "last user",
+                    "lockOwner": "Firstname Lastname",
+                    "locked": true,
+                    "remoteDocumentId": "bc1cea0f-93bd-44f2-8f35-16859da49e71",
+                    "reservationOwner": "owner lastname",
+                    "reserved": true
+                }
+                """)
         ));
     }
 
@@ -116,7 +118,7 @@ public class DeliusApiMockServer extends WireMockServer {
             .withStatus(201)
             .withBody("""
                 {
-                    "id": 2500029015,
+                    "id": 17666633,
                     "offenderCrn": "X320741",
                     "type": "CRSAPT",
                     "typeDescription": "Appointment with CRS Provider (NS)",
