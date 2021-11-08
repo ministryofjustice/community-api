26d6341f74096f726afd61ffe39c19754b9a193c
Ed Brindley <ed@maidavale.org>
2021-11-08 11:50:06 +0000

    Improve generated swagger docs for contact outcome API

--- src/main/java/uk/gov/justice/digital/delius/data/api/EnforcementAction.java
+++ src/main/java/uk/gov/justice/digital/delius/data/api/EnforcementAction.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.delius.data.api;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,11 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EnforcementAction {
+    @ApiModelProperty(required = true)
     private String code;
 
+    @ApiModelProperty(required = true)
     private String description;
 
+    @ApiModelProperty("Contact will be added to the enforcement diary")
     private Boolean outstandingContactAction;
 
+    @ApiModelProperty("Enforcement response date on the contact will be populated as this many days from the outcome date")
     private Long responseByPeriod;
 }
