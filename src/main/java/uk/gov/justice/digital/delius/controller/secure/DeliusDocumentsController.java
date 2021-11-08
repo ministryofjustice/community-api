405254c26323511c177e2b293f58b6e23df8a6a7
nataliewood <natalie.wood@digital.justice.gov.uk>
2021-11-08 12:35:03 +0000

    remove unused import

--- src/main/java/uk/gov/justice/digital/delius/controller/secure/DeliusDocumentsController.java
+++ src/main/java/uk/gov/justice/digital/delius/controller/secure/DeliusDocumentsController.java
@@ -5,7 +5,6 @@ import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
