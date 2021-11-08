e40146602cf3738cf28dd74febe4ae290029f3fc
Ed Brindley <ed@maidavale.org>
2021-11-08 12:39:31 +0000

    Improve generated swagger docs for contact outcome API (#573)

--- src/main/java/uk/gov/justice/digital/delius/data/api/ContactOutcomeTypeDetail.java
+++ src/main/java/uk/gov/justice/digital/delius/data/api/ContactOutcomeTypeDetail.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.delius.data.api;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,17 +13,24 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ContactOutcomeTypeDetail {
+    @ApiModelProperty(required = true)
     private String code;
 
+    @ApiModelProperty(required = true)
     private String description;
 
+    @ApiModelProperty(name = "Is this outcome compliant/acceptable")
     private Boolean compliantAcceptable;
 
+    @ApiModelProperty(name = "Does this outcome indicate attendance")
     private Boolean attendance;
 
+    @ApiModelProperty(name = "Is an enforcement action mandatory", required = true)
     private Boolean actionRequired;
 
+    @ApiModelProperty(name = "Can an enforcement action can be supplied")
     private Boolean enforceable;
 
+    @ApiModelProperty(name = "Available enforcement actions", required = true)
     private List<EnforcementAction> enforcements;
 }
