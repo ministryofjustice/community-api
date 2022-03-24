751904eadbdfbf550b954a20e6d77f85f289e0c1
Marcus Aspin <maspin@unilink.com>
2022-03-24 09:26:15 +0000

    PI-120 Populate required and completed dates for institutional reports (#620)

--- src/main/java/uk/gov/justice/digital/delius/jpa/standard/entity/InstitutionalReport.java
+++ src/main/java/uk/gov/justice/digital/delius/jpa/standard/entity/InstitutionalReport.java
@@ -5,8 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.time.LocalDate;
 
 @Data
 @Builder(toBuilder = true)
@@ -30,7 +37,13 @@ public class InstitutionalReport {
     private Long offenderId;
 
     @Column(name = "DATE_REQUESTED")
-    private LocalDateTime dateRequested;
+    private LocalDate dateRequested;
+
+    @Column(name = "DATE_REQUIRED")
+    private LocalDate dateRequired;
+
+    @Column(name = "DATE_COMPLETED")
+    private LocalDate dateCompleted;
 
     @ManyToOne
     @JoinColumn(name = "INSTITUTION_REPORT_TYPE_ID")
