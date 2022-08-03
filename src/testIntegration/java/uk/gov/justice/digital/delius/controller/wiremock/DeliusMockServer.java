b483d604bd67a6ba015aad59756fb6b246244a14
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-08-03 09:05:52 +0100

    removed case notes code

--- src/testIntegration/java/uk/gov/justice/digital/delius/controller/wiremock/DeliusMockServer.java
+++ src/testIntegration/java/uk/gov/justice/digital/delius/controller/wiremock/DeliusMockServer.java
@@ -10,28 +10,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
 
 public class DeliusMockServer extends WireMockServer {
 
-    public DeliusMockServer(final int port) {
-        super(WireMockConfiguration.wireMockConfig().port(port));
-    }
-
     public DeliusMockServer(final int port, final String fileDirectory) {
         super(WireMockConfiguration.wireMockConfig().port(port).usingFilesUnderDirectory(fileDirectory).jettyStopTimeout(10000L));
-    }
-
-    public void stubPutCaseNoteToDeliusCreated(final String nomisId, final Long caseNotesId) {
-        final var putCaseNote = "/nomisCaseNotes/" + nomisId + "/" + caseNotesId;
-        stubFor(put(urlPathMatching(putCaseNote)).willReturn(aResponse()
-            .withHeader("Content-Type", "application/json")
-            .withStatus(201)
-            .withBody(" XXXX (crn) had a Contact created.")
-        ));
-    }
-
-    public void stubPutCaseNoteToDeliusError(final String nomisId, final long caseNotesId, final HttpStatus status) {
-        final var putCaseNote = "/nomisCaseNotes/" + nomisId + "/" + caseNotesId;
-        stubFor(put(urlPathMatching(putCaseNote)).willReturn(aResponse()
-            .withHeader("Content-Type", "application/json")
-            .withStatus(status.value())
-        ));
     }
 }
