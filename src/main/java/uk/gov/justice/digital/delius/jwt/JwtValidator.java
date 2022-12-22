34b9cbe10755f55e6128b74f73394516947ccc32
Marcus Aspin <maspin@unilink.com>
2022-12-22 17:31:24 +0000

    PI-657 Manually set App Insights clientId after New Tech JWT validation

--- src/main/java/uk/gov/justice/digital/delius/jwt/JwtValidator.java
+++ src/main/java/uk/gov/justice/digital/delius/jwt/JwtValidator.java
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.delius.jwt;
 
 import com.google.common.collect.Lists;
+import com.microsoft.applicationinsights.web.internal.ThreadContext;
 import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -28,7 +29,6 @@ public class JwtValidator {
 
     @Before("execution(@uk.gov.justice.digital.delius.jwt.JwtValidation * *(..))")
     public void validateJwt(JoinPoint joinPoint) {
-
         ArrayList<Object> argsList = Lists.newArrayList(joinPoint.getArgs());
 
         Optional<Claims> maybeClaims = argsList.stream()
@@ -38,7 +38,11 @@ public class JwtValidator {
                 .map(jwt::parseAuthorizationHeader)
                 .orElseThrow(() -> new JwtTokenMissingException("No Authorization Bearer token found in headers."));
 
-        maybeClaims.ifPresent(CurrentUserSupplier::setClaims);
+        maybeClaims.ifPresent(claims -> {
+            CurrentUserSupplier.setClaims(claims);
+            ThreadContext.getRequestTelemetryContext().getHttpRequestTelemetry().getProperties()
+                .putIfAbsent("clientId", "delius-new-tech");
+        });
     }
 
 }
