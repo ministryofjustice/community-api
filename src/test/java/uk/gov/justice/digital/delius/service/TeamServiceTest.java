39f05beafa0cd0be6b38b08db805b94ca434c666
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-01-17 15:24:09 +0000

    fix tests after revert

--- src/test/java/uk/gov/justice/digital/delius/service/TeamServiceTest.java
+++ src/test/java/uk/gov/justice/digital/delius/service/TeamServiceTest.java
@@ -107,7 +107,7 @@ public class TeamServiceTest {
                         .code("N01")
                         .build())).isNotNull();
 
-        verify(teamRepository).findByCode("N01UAT");
+        verify(teamRepository).findByCode("N01ALL");
     }
 
     @Test
